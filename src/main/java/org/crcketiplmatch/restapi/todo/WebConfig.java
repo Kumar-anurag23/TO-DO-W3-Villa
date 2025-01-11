3d6174bc97dd36fe70f5a1e6c8a4fa4e0691fa4d
Kumar-anurag23 <akguyra@gmail.com>
2025-01-11 16:36:39 +0530

    fix

--- src/main/java/org/crcketiplmatch/restapi/todo/WebConfig.java
+++ src/main/java/org/crcketiplmatch/restapi/todo/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all endpoints
-                .allowedOrigins("http://localhost:8080")  // Allow specific origin
+                .allowedOrigins("*")  // Allow specific origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
